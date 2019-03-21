e2a0c3a681d424ddb7bcfe0473abbbf9b7f83a83
Mohammad Jawwad <mjawwad@stuy.edu>
2019-03-21 14:48:48 -0400

    updated toString with testing + added first addLast()

--- MyDeque.java
+++ MyDeque.java
@@ -5,11 +5,15 @@ public class MyDeque<E>{
   @SuppressWarnings("unchecked")
   public MyDeque(){
     data = (E[])new Object[10];
+    start = 5;
+    end = 5;
   }
 
   @SuppressWarnings("unchecked")
   public MyDeque(int initialCapacity){
     data = (E[]) new Object[initialCapacity];
+    start = initialCapacity / 2;
+    end = initialCapacity /2;
   }
 
   public int size(){
@@ -24,29 +28,50 @@ public class MyDeque<E>{
   public String toString(){
     String out = "[";
     if (end < start) {
-      for (int i = start; i < data.length; i++) {
+      for (int i = start +1; i < data.length; i++) {
         out += data[i] + ", ";
       }
-      for (int j = 0; j < e; j++) {
-        if (j != e -1)  out += data[j] + ", ";
+      for (int j = 0; j < end +1; j++) {
+        if (j != end)  out += data[j] + ", ";
         else out += data[j];
       }
     }
     else {
-      for (int i = start; i < e; i++) {
-        if (i != e -1) out += data[j] + ", ";
-        else out += data[j];
+      for (int i = start +1; i < end + 1; i++) {
+        if (i != end) out += data[i] + ", ";
+        else out += data[i];
       }
     }
     out += "]";
     return out;
+  }
 
 
 
-      public void addFirst(E element){ }
-  public void addLast(E element){ }
-  public E removeFirst(){ }
+  public void addFirst(E element){}
+  public void addLast(E element){
+    if (end != data.length - 1) {
+      data[end +1] = element;
+      end++;
+    }
+    else {
+      end = 0;
+      data[end] = element;
+      end++;
+    }
+  }
+  /*public E removeFirst(){ }
   public E removeLast(){ }
   public E getFirst(){ }
-  public E getLast(){ }
+  public E getLast(){ }*/
+
+
+  public static void main(String[] args) {
+    MyDeque<Integer> s = new MyDeque();
+    s.addLast(5);
+    s.addLast(12);
+    System.out.println(s.start); //still 5
+    System.out.println(s.end); //now 7
+    System.out.println(s); //[5,12]
+  }
 }
