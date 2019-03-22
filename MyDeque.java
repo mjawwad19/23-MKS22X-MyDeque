c3b695b60579e5ca36e3c69b3ad6ea3c85669d25
Mohammad Jawwad <mjawwad@stuy.edu>
2019-03-22 12:26:16 -0400

    added resize/improved toString

--- MyDeque.java
+++ MyDeque.java
@@ -5,60 +5,97 @@ public class MyDeque<E>{
   @SuppressWarnings("unchecked")
   public MyDeque(){
     data = (E[])new Object[10];
-    start = 5;
-    end = 5;
+    start = 0;
+    end = 0;
+    size = 0;
   }
 
   @SuppressWarnings("unchecked")
   public MyDeque(int initialCapacity){
     data = (E[]) new Object[initialCapacity];
-    start = initialCapacity / 2;
-    end = initialCapacity /2;
+    start = 0;
+    end = 0;
+    size = 0;
   }
 
   public int size(){
-    int s;
-    if (end < start) {
-      s = (data.length - start) + (end);
-    }
-    else s = end - start;
-    return s;
+    return size;
   }
 
   public String toString(){
     String out = "[";
-    if (end < start) {
-      for (int i = start +1; i < data.length; i++) {
-        out += data[i] + ", ";
-      }
-      for (int j = 0; j < end +1; j++) {
-        if (j != end)  out += data[j] + ", ";
-        else out += data[j];
+    if (size == 0) return "[]";
+    if (start <= end) {
+      for (int i = start; i <= end; i++) {
+        out += data[i];
+        if (i != end) out += ", ";
       }
     }
     else {
-      for (int i = start +1; i < end + 1; i++) {
-        if (i != end) out += data[i] + ", ";
-        else out += data[i];
+      for (int i = 1; i < size; i++) {
+        if (i + start < data.length) {
+          out += data[i+start];
+        }
+        else {
+          out += data[i+start-data.length];
+        }
+        if (i+ start - data.length != end -1) {
+          out += ", ";
+        }
       }
     }
     out += "]";
     return out;
   }
 
+  @SuppressWarnings("unchecked")
+  private void resize() {
+    E[] n = (E[]) new Object[(data.length-1) * 2];
+    int in = size -start;
+    if (start <= end) {
+      for (int i = start; i <= end; i++) {
+        n[i] = data[i]; //copy over
+      }
+    }
+    else {
+      for (int i = 0; i < size; i++) {
+        if (i + start < data.length) {
+          n[n.length - (in--)] = data[i+start];
+        }
+        else {
+          n[i + start - data.length] = data[i + start - data.length];
+          //other method of copying over when end is not after start
+        }
+      }
+      start = n.length - (size - start);
+    }
+    data = n;
+  }
+
 
 
   public void addFirst(E element){}
   public void addLast(E element){
-    if (end != data.length -1 && end != start-1) {
-      data[end +1] = element;
+    if (size == 0) data[end] = element; //free for all
+    else if (size == data.length) {
+      resize();
       end++;
+      data[end] = element; //necessary since no avaliable space anywehere
     }
-    else if (start != 0) {
-      end = 0;
-      data[end] = element;
+    else if (start > end) {
+      end++;
+      data[end] = element; //keep going
     }
-    else System.out.println("no space");
+    else {
+      if (end == data.length -1) {
+        end = 0;
+        data[0] = element; //put to front when space avail @ front
+      }
+      else {
+        end++;
+        data[end] = element; //regular
+      }
+    }
   }
   /*public E removeFirst(){ }
   public E removeLast(){ }
