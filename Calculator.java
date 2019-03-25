e6cc473ea41ac0f5b9c9fbef500583e77e875a3d
Mohammad Jawwad <mjawwad@stuy.edu>
2019-03-25 17:24:13 -0400

    removed main... moving to a new Driver file

--- Calculator.java
+++ Calculator.java
@@ -13,7 +13,6 @@ public class Calculator{
     }
     // based on the operator token, perform the operation on two given doubles
     private static double operate(double a, double b, String op) {
-      //System.out.println(b + op + a);
       if (op.equals("+")) return b + a;
       else if (op.equals("-")) return b - a;
       else if (op.equals("*")) return b * a;
@@ -25,7 +24,6 @@ public class Calculator{
       String[] tokens = s.split(" ");
       MyDeque<Double> stack = new MyDeque<Double>();
       for (int i = 0; i < tokens.length; i++) {
-        //System.out.println(tokens[i]);
         //if the token is a value
         if (!isOp(tokens[i])) {
           //for the cases where we have multiple values before operations to add to the stack and whittle down
@@ -35,21 +33,8 @@ public class Calculator{
           double ans = operate(stack.removeLast(), stack.removeLast(), tokens[i]);
           stack.addLast(ans); //removes the last two stacks and adds their result as a new stack
         }
-        //System.out.println(stack);
       }
       //eventually you will have one stack left: the answer
       return stack.removeFirst();
-    }
-
-
-    public static void main(String[] args) {
-      System.out.println(eval("10 2.0 +")); //is 12.0 CORRECT
-      System.out.println();
-      System.out.println(eval("11 3 - 4 + 2.5 *")); //is 30.0 CORRECT
-      System.out.println();
-      System.out.println(eval("8 2 + 99 9 - * 2 + 9 -")); //is 893.0 CORRECT
-      System.out.println();
-      System.out.println(eval("1 2 3 4 5 + * - -")); //is 26.0 CORRECT
-      System.out.println();
     }
 }
