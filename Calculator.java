390886c1bd8ab6849da3f715181340efe93c6092
Mohammad Jawwad <mjawwad@stuy.edu>
2019-03-25 17:19:57 -0400

    added comments/ simplified the runtime for isOp from n to constant.

--- Calculator.java
+++ Calculator.java
@@ -2,17 +2,18 @@ public class Calculator{
     /*Evaluate a postfix expression stored in s.
      *Assume valid postfix notation, separated by spaces.
      */
-    private static String[] ops = {"+", "-", "*", "/", "%"};
-
+    //returns if the token is an operator (true) or a value (false)
     private static boolean isOp(String token) {
-      for (int i = 0; i < ops.length; i++) {
-        if (token.equals(ops[i])) return true;
-      }
-      return false;
+      return (token.equals("+") ||
+          token.equals("-") ||
+          token.equals("*") ||
+          token.equals("/") ||
+          token.equals("%"));
+          //now much simpler and not looping through an array of the ops... n time
     }
-
+    // based on the operator token, perform the operation on two given doubles
     private static double operate(double a, double b, String op) {
-      System.out.println(b + op + a);
+      //System.out.println(b + op + a);
       if (op.equals("+")) return b + a;
       else if (op.equals("-")) return b - a;
       else if (op.equals("*")) return b * a;
@@ -25,16 +26,18 @@ public class Calculator{
       MyDeque<Double> stack = new MyDeque<Double>();
       for (int i = 0; i < tokens.length; i++) {
         //System.out.println(tokens[i]);
-        //if the token is an operator
+        //if the token is a value
         if (!isOp(tokens[i])) {
+          //for the cases where we have multiple values before operations to add to the stack and whittle down
           stack.addLast(Double.parseDouble(tokens[i]));
         }
-        else {
+        else { //when the token is an operator
           double ans = operate(stack.removeLast(), stack.removeLast(), tokens[i]);
-          stack.addLast(ans);
+          stack.addLast(ans); //removes the last two stacks and adds their result as a new stack
         }
-        System.out.println(stack);
+        //System.out.println(stack);
       }
+      //eventually you will have one stack left: the answer
       return stack.removeFirst();
     }
 
@@ -47,5 +50,6 @@ public class Calculator{
       System.out.println(eval("8 2 + 99 9 - * 2 + 9 -")); //is 893.0 CORRECT
       System.out.println();
       System.out.println(eval("1 2 3 4 5 + * - -")); //is 26.0 CORRECT
+      System.out.println();
     }
 }
